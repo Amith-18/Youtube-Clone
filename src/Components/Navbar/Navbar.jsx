6aebf1163a5f616e88c5115a317ba3ed66763204
Amith-18 <amithsanil@outlook.com>
2025-01-03 18:35:00 +0530

    navbar css for nav-left

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -9,27 +9,30 @@ import notification_icon from '../../assets/notification.png'
 import profile_icon from '../../assets/jack.png'
 
 const Navbar = () => {
-  return (
-    <nav className='flex-div'>
-        <div className='nav-left flex-div'>
-            <img className='menu-icon' src={menu_icon} alt="" />
-            <img className='logo' src={logo} alt="" />
-        </div>
+    return (
+        <nav className='flex-div'>
+            <div className='nav-left flex-div'>
+                <img className='menu-icon' src={menu_icon} alt="" />
+                <img className='logo' src={logo} alt="" />
+            </div>
 
-        <div className='nav-middle flex-div'>
-            <input type="text" placeholder='Search'/>
-            <img src={Search_icon} alt="" />
-        </div>
+            <div className='nav-middle flex-div'>
+                <div className='Search-box flex-div'>
+                    <input type="text" placeholder='Search' />
+                    <img src={Search_icon} alt="" />
+                </div>
 
-        <div className='nav-right flex-div'>
-            <img src={upload_icon} alt=""></img>
-            <img src={more_icon} alt=""></img>
-            <img src={notification_icon} alt=""></img>
-            <img className='user-icon' src={profile_icon} alt=""></img>
-        </div>
-        
-    </nav>
-  )
+            </div>
+
+            <div className='nav-right flex-div'>
+                <img src={upload_icon} alt=""></img>
+                <img src={more_icon} alt=""></img>
+                <img src={notification_icon} alt=""></img>
+                <img className='user-icon' src={profile_icon} alt=""></img>
+            </div>
+
+        </nav>
+    )
 }
 
 export default Navbar
