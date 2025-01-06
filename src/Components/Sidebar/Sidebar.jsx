ca12f0a9cdbeb919c69653d4956c77f9120ea767
Amith-18 <amithsanil@outlook.com>
2025-01-06 21:42:07 +0530

    Added Side-link

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -48,6 +48,24 @@ const Sidebar = () => {
         </div>
         <hr />
       </div>
+      <div className="subscriber-list">
+        <h3>Subscribed</h3>
+        <div className="side-link">
+          <img src={jack} alt="" /><p>PewDiePie</p>
+        </div>
+        <div className="side-link">
+          <img src={simon} alt="" /><p>MrBeast</p>
+        </div>
+        <div className="side-link">
+          <img src={tom} alt="" /><p>Justin Bieber</p>
+        </div>
+        <div className="side-link">
+          <img src={megan} alt="" /><p>5 Minute Craft</p>
+        </div>
+        <div className="side-link">
+          <img src={cameron} alt="" /><p>Nas Daily</p>
+        </div>
+      </div>
     </div>
   )
 }
