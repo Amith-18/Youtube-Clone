9e283330916f892be1a778fd45725f9c68af286e
Amith-18 <amithsanil@outlook.com>
2025-01-08 22:43:46 +0530

    changes in playvideo

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -31,6 +31,96 @@ const PlayVideo = () => {
                 </div>
                 <button>Subscribe</button>
             </div>
+            <div className="vid-desc">
+                <p>Channel that makes learning Easy</p>
+                <p>Subscribe Amith to watch More Tutorial about 3D printing</p>
+                <hr />
+                <h4>222 Comments</h4>
+                <div className="comments">
+                    <img src={user_profile} alt="" />
+                    <div>
+                        <h3>Jack <span>1 day ago</span></h3>
+                        <p>Very Impressive video</p>
+                        <div className='comment-action'>
+                            <img src={like} alt="" />
+                            <span>222</span>
+                            <img src={dislike} alt="" />
+                        </div>
+                    </div>
+                </div>
+                <div className="comments">
+                    <img src={user_profile} alt="" />
+                    <div>
+                        <h3>Jack <span>1 day ago</span></h3>
+                        <p>Very Impressive video</p>
+                        <div className='comment-action'>
+                            <img src={like} alt="" />
+                            <span>222</span>
+                            <img src={dislike} alt="" />
+                        </div>
+                    </div>
+                </div>
+                <div className="comments">
+                    <img src={user_profile} alt="" />
+                    <div>
+                        <h3>Jack <span>1 day ago</span></h3>
+                        <p>Very Impressive video</p>
+                        <div className='comment-action'>
+                            <img src={like} alt="" />
+                            <span>222</span>
+                            <img src={dislike} alt="" />
+                        </div>
+                    </div>
+                </div>
+                <div className="comments">
+                    <img src={user_profile} alt="" />
+                    <div>
+                        <h3>Jack <span>1 day ago</span></h3>
+                        <p>Very Impressive video</p>
+                        <div className='comment-action'>
+                            <img src={like} alt="" />
+                            <span>222</span>
+                            <img src={dislike} alt="" />
+                        </div>
+                    </div>
+                </div>
+                <div className="comments">
+                    <img src={user_profile} alt="" />
+                    <div>
+                        <h3>Jack <span>1 day ago</span></h3>
+                        <p>Very Impressive video</p>
+                        <div className='comment-action'>
+                            <img src={like} alt="" />
+                            <span>222</span>
+                            <img src={dislike} alt="" />
+                        </div>
+                    </div>
+                </div>
+                <div className="comments">
+                    <img src={user_profile} alt="" />
+                    <div>
+                        <h3>Jack <span>1 day ago</span></h3>
+                        <p>Very Impressive video</p>
+                        <div className='comment-action'>
+                            <img src={like} alt="" />
+                            <span>222</span>
+                            <img src={dislike} alt="" />
+                        </div>
+                    </div>
+                </div>
+                <div className="comments">
+                    <img src={user_profile} alt="" />
+                    <div>
+                        <h3>Jack <span>1 day ago</span></h3>
+                        <p>Very Impressive video</p>
+                        <div className='comment-action'>
+                            <img src={like} alt="" />
+                            <span>222</span>
+                            <img src={dislike} alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
