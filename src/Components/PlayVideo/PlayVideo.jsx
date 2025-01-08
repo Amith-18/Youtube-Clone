a04716f3a16b9f88da9eb5f1a2c6ec2808c3a058
Amith-18 <amithsanil@outlook.com>
2025-01-08 22:31:38 +0530

    changes in playVideo

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -9,12 +9,23 @@ import jack from '../../assets/jack.png'
 import user_profile from '../../assets/user_profile.jpg'
 
 const PlayVideo = () => {
-  return (
-    <div className='play-video'>
-        <video src={video1}></video>
-    
-    </div>
-  )
+    return (
+        <div className='play-video'>
+            <video src={video1} controls autoPlay muted></video>
+            <h3>Best Youtube channel To Learn Web Development</h3>
+            <div className='play-video-info'>
+                <p>222 views &bull; 2 days ago</p>
+                <div>
+                    <span><img src={like} alt="" />125</span>
+                    <span><img src={dislike} alt="" />2</span>
+                    <span><img src={share} alt="" />Share</span>
+                    <span><img src={save} alt="" />Save</span>
+                </div>
+            </div>
+            <hr />
+            
+        </div>
+    )
 }
 
 export default PlayVideo
